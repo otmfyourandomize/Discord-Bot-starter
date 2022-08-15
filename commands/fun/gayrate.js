b6b76d6f649a7d0a14a6a95b37dfe706fa5920ff
Minesraft2 <aerellmck@gmail.com>
2022-08-14 21:36:31 -0500

    Add gayrate command

---
+++ commands/fun/gayrate.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('gayrate')
+        .setDescription("Rates how gay someone is")
+        .addUserOption(opt => opt.setName('target').setDescription("Person to rate")),
+    category: "fun",
+    async execute(client, interaction, args) {
+        await interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setTitle("Gay Rate")
+                    .setDescription(`${(args.target ? interaction.guild.members.fetch(args.target) : interaction.member).toString()} is ${args.target == "600356507829141544" ? 0 : Math.floor(Math.random()*100)}% gay`)
+                    .setColor("2F3136")
+            ]
+        });
+    },
+};
