f8ee6c5fb38cc959465e0d1c657ad05f80d2cd6b
Minesraft2 <aerellmck@gmail.com>
2022-08-15 17:21:28 -0500

    add /avatar command

---
+++ commands/info/avatar.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('avatar')
+        .setDescription('Gets member avatar')
+        .addUserOption(opt => opt.setName("member").setDescription("Member to get avatar of")),
+    usage: "/avatar [member]",
+    category: "info",
+    async execute(client, interaction, args) {
+        let target;
+        try {
+            target = args.member ? await interaction.guild.members.fetch(args.member) : interaction.member;
+        } catch {
+            target = interaction.member;
+        }
+        await interaction.reply({ files: [new AttachmentBuilder().setName("avatar.png").setFile(target.displayAvatarURL({ extension: "png", dynamic: true, size: 4096 }))], ephemeral: true });
+    },
+};
