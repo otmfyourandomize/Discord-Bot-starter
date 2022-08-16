80e2246a79cc4403ed3468b462f8d9c9cf060406
Minesraft2 <aerellmck@gmail.com>
2022-08-16 06:20:25 -0500

    Server invite

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -8,6 +8,6 @@ module.exports = {
     category: "info",
     async execute(client, interaction, args) {
         var url = `https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=1644971949559&scope=bot%20applications.commands`;
-        await interaction.reply({ content: `[Click here to invite bot](${url})`, ephemeral: true });
+        await interaction.reply({ content: `[Bot Invite](${url})\n[Server Invite](<https://discord.gg/${process.env.SERVER_INV}> "Kingdom of Jod")`, ephemeral: true });
     },
 };
